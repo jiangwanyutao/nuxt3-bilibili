54143fdb2576c9223d55bfb63f1b6ffb6b018f4f
江晚正愁余 <1015134040@qq.com>
2024-06-06 18:25:50 +0800

    feat:改配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: 'src',
   //调试工具
   devtools: { enabled: true },
   // 是否开启 ssr 服务端渲染
-  ssr: true,
+  ssr: false,
   // 应用模块
   modules: ['@vant/nuxt'],
    // 移动端适配
@@ -14,5 +13,23 @@ export default defineNuxtConfig({
         viewportWidth: 375,
       },
     },
+  },
+  routeRules: {
+    // all routes (by default) will be revalidated every 60 seconds, in the background
+    '/**': { isr: 60 },
+    // this page will be generated on demand and then cached permanently
+    '/static': { isr: true },
+    // this page is generated at build time and cached permanently
+    '/prerendered': { prerender: true },
+    // this page will be always fresh
+    '/dynamic': { isr: false },
+    // you can do lots more with route rules too!
+    '/redirect': { redirect: '/static' },
+    '/headers': { headers: { 'x-magic-of': 'nuxt and vercel' } },
+    '/spa': { ssr: false },
+  },
+  runtimeConfig: {
+    nitro: { envPrefix: 'VERCEL_' },
+    region: process.env.VERCEL_REGION,
   },
 })
